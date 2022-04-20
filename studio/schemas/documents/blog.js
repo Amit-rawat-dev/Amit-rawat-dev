301952b0c10b87501e400122bc56d4eb43365ed3
Amit-rawat-dev <amit.r@srmtechsol.com>
2022-04-20 16:49:23 +0530

    Sending full project data

---
+++ studio/schemas/documents/blog.js
@@ -0,0 +1,15 @@
+import { GitNotebook } from 'react-icons/gi';
+
+export default {
+  title: 'Blog Post',
+  name: 'blog',
+  type: 'document',
+  icon: GitNotebook,
+  fields: [
+    {
+      title: 'Title',
+      name: 'title',
+      type: 'string',
+    },
+  ],
+};
---
+++ studio/schemas/schema.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import createSchema from 'part:@sanity/base/schema-creator';
+import schemaTypes from 'all:part:@sanity/base/schema-type';
+/* eslint-enable */
+
+// import custom schemas
+
+import blog from './documents/blog';
+
+export default createSchema({
+  name: 'default',
+  types: schemaTypes.concat([blog]),
+});
